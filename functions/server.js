9a456f3a0152f91385fb5f7a758c0284062b8ceb
ahmed <gfg59204@gmail.com>
2024-05-25 12:26:28 -0700

    first commit

--- functions/server.js
+++ functions/server.js
@@ -1,10 +1,6 @@
 import express from "express";
 import serverless from "serverless-http";
 import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const router = express.Router();
@@ -15,47 +11,56 @@ app.use(express.static('public'));
 // Set View's
 app.set('views', './views');
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Navigation
 router.get('/', (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs");
 });
+
 router.get('/login', (req, res) => {
-    res.render("login.ejs");
+  res.render("login.ejs");
 });
+
 router.get('/home', (req, res) => {
-    res.render("home.ejs");
+  res.render("home.ejs");
 });
+
 router.get('/register', (req, res) => {
-    res.render("register.ejs");
+  res.render("register.ejs");
 });
+
 router.get('/institution', (req, res) => {
-    res.render("institution.ejs");
+  res.render("institution.ejs");
 });
+
 router.get('/instEx', (req, res) => {
-    res.render("instEx.ejs");
+  res.render("instEx.ejs");
 });
+
 router.get('/FAQs', (req, res) => {
-    res.render("FAQs.ejs");
+  res.render("FAQs.ejs");
 });
+
 router.get('/myprofile', (req, res) => {
-    res.render("myprofile.ejs");
+  res.render("myprofile.ejs");
 });
+
 router.get('/mycert', (req, res) => {
-    res.render("mycert.ejs");
+  res.render("mycert.ejs");
 });
+
 router.get('/cv', (req, res) => {
-    res.render("mycv.ejs");
+  res.render("mycv.ejs");
 });
+
 router.get('/certificate', (req, res) => {
-    res.render("certificate.ejs");
+  res.render("certificate.ejs");
 });
+
 router.get('/certEx', (req, res) => {
-    res.render("certEx.ejs");
+  res.render("certEx.ejs");
 });
 
 app.use('/.netlify/functions/server', router);
-
 export const handler = serverless(app);
